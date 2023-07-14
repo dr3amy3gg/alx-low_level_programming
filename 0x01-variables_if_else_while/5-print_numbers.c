60ca09bea7d424a8fdceb27ab69d10309103a2d5
dr3amy3gg <rylin.jansen@gmail.com>
2023-07-14 10:33:15 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints single digits
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
