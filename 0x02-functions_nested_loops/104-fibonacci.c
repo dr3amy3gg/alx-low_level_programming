6f7279ded45ab4a29f799efd5bbd91278bc392c9
dr3amy3gg <rylin.jansen@gmail.com>
2023-07-18 13:03:42 -0700

    rework

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,26 +1,30 @@
 #include <stdio.h>
-
 /**
- * main - entry point
- * Return: 0
+ * main - main function
+ *
+ * Return: nothing
  */
-
 int main(void)
 {
+	int counter = 2;
+
 	float a = 1;
-	float b = a;
+	float b = a + 1;
 	float c = a + b;
-	int index = 1;
 
-	printf("%.0f", b);
-
-	while (index < 98)
+	printf("%.0f, ", a);
+	printf("%.0f, ", b);
+	while (counter < 98)
 	{
-		printf(", %.0f", c);
+		counter++;
+		printf("%.0f", c);
 		a = b;
 		b = c;
 		c = a + b;
-		index++;
+		if (counter < 98)
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 	return (0);
