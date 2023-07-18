fb1e743b9352daf78d73559c8c31d1f7ca96d96c
dr3amy3gg <rylin.jansen@gmail.com>
2023-07-18 13:18:10 -0700

    broke up larger numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,27 +1,50 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - print first 98 Fibonacci numbers without using long long, malloc,
+ * pointers, array/tables, or structures
  * Return: 0
  */
 
 int main(void)
 {
-	unsigned int a = 1;
-	unsigned int b = a;
-	unsigned int c = a + b;
-	int index = 1;
+	int counter, overflow;
+	unsigned long a = 1;
+	unsigned long b = 1;
+	unsigned long sum = 0;
+	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
 
-	printf("%X", b);
+	printf("1");
 
-	while (index < 98)
+	for (counter = 2; counter < 93; counter++) /* will still print */
 	{
-		printf(", %X", c);
+		sum = a + b;
 		a = b;
-		b = c;
-		c = a + b;
-		index++;
+		b = sum;
+		printf(", %lu", sum);
 	}
+
+	a_head = a / 1000000000; /* break larger num into 2 parts */
+	a_tail = a % 1000000000;
+	b_head = b / 1000000000;
+	b_tail = b % 1000000000;
+
+	for (; counter < 99; counter++)
+	{
+		overflow = (a_tail + b_tail) / 1000000000;
+		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
+		sum_head = (a_head + b_head) + overflow;
+
+		printf(", %lu%lu", sum_head, sum_tail);
+
+		a_head = b_head;
+		a_tail = b_tail;
+		b_head = sum_head;
+		b_tail = sum_tail;
+	}
+
+
 	printf("\n");
+
 	return (0);
 }
