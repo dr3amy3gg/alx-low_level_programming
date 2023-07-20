4b43007795e2f1b7c954f9d5ae75f1e138fcc5bb
dr3amy3gg <rylin.jansen@gmail.com>
2023-07-20 06:26:42 -0700

    forgot prototype

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -3,3 +3,10 @@
  * @c: character to put to standard output
  */
 int _putchar(char c);
+
+/**
+ * _isupper - checks for uppercase letters
+ * @c: input
+ * Return: 1 for true, 0 for false
+ */
+int _isupper(int c);
