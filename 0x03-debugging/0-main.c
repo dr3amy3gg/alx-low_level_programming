0148c232cb2fb8d6a3d6d23b76bbfd6f9129eb11
dr3amy3gg <rylin.jansen@gmail.com>
2023-07-20 00:59:40 -0700

    multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * description: tests whether integer is positive or negative
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	int = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+0 create a file that tests that the function positive_or_negative() gives the correct output  
