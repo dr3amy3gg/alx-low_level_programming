b0d4169957ecf45cbb5eaac155a04d9884de0d09
dr3amy3gg <rylin.jansen@gmail.com>
2023-07-31 02:56:52 -0700

    forgot underscore in prototype - not even for the one i'm currently doing!

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@ int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
 
-char *strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
 
