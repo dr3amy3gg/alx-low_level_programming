52958f0b6a030f2083323c6e98ea5f039c8ff37f
dr3amy3gg <rylin.jansen@gmail.com>
2023-07-31 11:15:17 -0700

    used while loop

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,17 +9,19 @@
 
 char *_strchr(char *s, char c)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	while (s[i] != '\0' && s[i] != c)
 	{
-		if (s[i] == c)
-		{
-			return (&s[i]);
-		}
-		else
-		{
-			return (NULL);
-		}
+		i++;
+	}
+
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+	else
+	{
+		return (NULL);
 	}
 }
