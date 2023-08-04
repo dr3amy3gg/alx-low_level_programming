0ca643f276d40d8211ced3f99f0fb627af7ef848
dr3amy3gg <rylin.jansen@gmail.com>
2023-08-04 07:05:41 -0700

    unused attribute error fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 
 /** main - prints name of program
  * Return: 0 (success)
  */
 
-int main(int argc, char* argv[])
+int main(int __attribute__((unused)) argc, char* argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
